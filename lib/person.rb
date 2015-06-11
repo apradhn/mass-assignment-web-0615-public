a345438c2fb390399317f5b79da995efb55669ba
apradhn <abhijit.pradhan6@gmail.com>
2015-06-11 15:29:48 -0400

    all done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 class Person
   # code here
+  
+  def initialize(properties)
+    properties.each do |key, value|
+      instance_variable_set("@#{key.to_s}", value)
+      define_singleton_method(key) { value }
+    end
+  end  
+
 end
